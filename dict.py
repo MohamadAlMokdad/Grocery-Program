538cbbcc0f0d2ee64853526b810fc06cc1e7ff08
Mohamad Al Mokdad <mohammad77.2002@gmail.com>
2025-09-29 20:43:05 +0300

    Full program

--- dict.py
+++ dict.py
@@ -8,9 +8,9 @@ groceries = {
 shop = []
 
 while True:
-  grocery = input("What do you want to buy(If you want to stop enter done.)")
+  grocery = input("What do you want to buy.If you want to stop enter done: ")
 
-  if grocery.lower == "done":
+  if grocery.lower() == "done" :
     break
   
   elif grocery in groceries:
@@ -20,5 +20,15 @@ while True:
     print("Not found in groceries")
 
 
+total = 0 
 
+for i in shop:
+  total += groceries[i]
 
+print("You bought: ",shop)
+print("Total: $", total)
+
+if total > 10 :
+  print("You spent a lot!")
+else:
+  print("You spent a little!")
